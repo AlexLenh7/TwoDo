ac7f88b5e478e6b4694dcaa20dafde41cdabce99
AlexLenh7 <alexlenh666@gmail.com>
2026-04-04 04:29:38 -0700

    initial project setup

---
+++ backend/src/test/java/com/TwoDo/TwoDo/TwoDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TwoDo.TwoDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
